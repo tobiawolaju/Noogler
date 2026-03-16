c0869824442140fefc14a642e9545dee736dccd6
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-03-16 20:45:51 +0100

    Update App.svelte

--- frontend/src/chat/App.svelte
+++ frontend/src/chat/App.svelte
@@ -14,7 +14,7 @@
     : "User";
 
   let ws;
-  let wsUrl = "ws://127.0.0.1:8080";
+  let wsUrl = "wss://noogler-265815053881.europe-west1.run.app";
   let connected = false;
   let lastError = "";
   let reconnectTimer;
